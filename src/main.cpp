1a35cf6cd803af3ab105b846cbe24b4bffb20e55
stevendee37 <stevenwchang01@gmail.com>
2025-01-25 12:40:41 -0800

    Implemented basic framework for capturing and processing images from camera.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,40 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main() {
-    cv::VideoCapture capture(0);
-    
-    cv::Mat img;
+using namespace std;
+using namespace cv;
 
-    capture >> img;
-    cv::imshow("Image", img); // Display the image in a window
-    cv::waitKey(0);             // Wait for a key press
+int main() {
+    VideoCapture cap;
+
+    if(!cap.open(0)){
+        cout << "Failed to open camera" << endl;
+    }
+    
+    Mat frame, grey_frame;
+    vector<Mat> frames; 
+
+    while(true){
+        cap >> frame;
+        if(frame.empty()){
+            break;
+        }
+        imshow(frame);
+        // On pressing space, continue
+        if(waitkey(10)==32){
+            break;
+        }
+    }
+    
+    // Convert frames to grayscale, add them to vector of frames for processing
+    while frames.size() < 1:
+        cap >> frame; 
+        cvtColor(frame, grey_frame, COLOR_BGR2GRAY);
+        frames.push_back(frame);
+    
+    for(int i = 0; i < frames.size(); i++){
+        unsigned char* image = frames.data();
+        // TODO: Gaussian blur image
+    }
     return 0;
 }
