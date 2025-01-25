73f7b9bd9e3dc3601cc0b9d1d9e231367e5a386c
stevendee37 <stevenwchang01@gmail.com>
2025-01-25 12:48:23 -0800

    Updated video frame input

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,30 @@ int main() {
         cout << "Failed to open camera" << endl;
     }
     
-    Mat frame, grey_frame;
+    Mat frame, gray_frame;
     vector<Mat> frames; 
+    unsigned char *img;
 
     while(true){
         cap >> frame;
         if(frame.empty()){
             break;
         }
-        imshow(frame);
+        imshow("Camera Feed", frame);
         // On pressing space, continue
-        if(waitkey(10)==32){
+        if(waitKey(10)==32){
             break;
         }
     }
     
     // Convert frames to grayscale, add them to vector of frames for processing
-    while frames.size() < 1:
+    while(frames.size() < 1){
         cap >> frame; 
-        cvtColor(frame, grey_frame, COLOR_BGR2GRAY);
-        frames.push_back(frame);
-    
+        cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
+        frames.push_back(gray_frame.clone());
+    }
     for(int i = 0; i < frames.size(); i++){
-        unsigned char* image = frames.data();
+        img = frames[i].data;
         // TODO: Gaussian blur image
     }
     return 0;
