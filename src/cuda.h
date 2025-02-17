10b0a043fee04989e16ffeec90ce2980b9c679d1
Steven Chang <stevenwchang01@gmail.com>
2025-02-17 12:03:30 -0800

    Reorganized directory for cuda

---
+++ src/cuda.h
@@ -0,0 +1,6 @@
+#ifndef CUDA_H
+#define CUDA_H
+
+void gaussian(unsigned char* img, float sigma, int rows, int columns, short int*& result);
+
+#endif
---
+++ tests/cuda/test_cuda.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
+
+
+TEST(CudaGaussian, Works){
+    EXPECT_EQ(true, false);
+}
