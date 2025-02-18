3d7b3744dca7658d1c6f6391826410ab068d219c
Steven Chang <stevenwchang01@gmail.com>
2025-02-17 18:26:11 -0800

    Implemented other cuda gaussian tests

--- tests/cuda/test_cuda.cpp
+++ tests/cuda/test_cuda.cpp
@@ -27,7 +27,28 @@ TEST(CudaGaussian, IsNonzero){
     clear_memory(result);
 }
 
-TEST(CudaGaussian, Visual_Test){
+TEST(CudaGaussian, InRange){
+    std::string image_path = std::string(PROJECT_DIR) + "/tests/test.jpg";
+    Mat img = cv::imread(image_path, IMREAD_GRAYSCALE);
+    short int* result;
+    unsigned char* data = img.data;
+    float sigma = 0.5;
+    int rows = 256;
+    int columns = 256;
+  
+    int sum = 0;
+  
+    gaussian(data,sigma,rows,columns,result);
+  
+    for(int i = 0; i < (rows*columns); i++){
+      EXPECT_LE(result[i], 255);
+      EXPECT_GE(result[i], 0);
+    }
+  
+    clear_memory(result);
+}
+  
+TEST(CudaGaussian, GaussianDimensions){
     std::string image_path = std::string(PROJECT_DIR) + "/tests/test.jpg";
     Mat img = cv::imread(image_path, IMREAD_GRAYSCALE);
     short int* result;
@@ -40,12 +61,32 @@ TEST(CudaGaussian, Visual_Test){
   
     gaussian(data,sigma,rows,columns,result);
 
-    Mat resultMat(256,256, CV_16S, result);
-    Mat result_display;
+    Mat smoothedMat(rows, columns, CV_16S, result);
+    EXPECT_EQ(smoothedMat.rows, rows);
+    EXPECT_EQ(smoothedMat.cols, columns);
 
-    normalize(resultMat, result_display, 0, 255, NORM_MINMAX);
-    result_display.convertTo(result_display, CV_8U);
-
-    imshow("CudaGaussian Visual Test", result_display);
-    waitKey(0);
+    clear_memory(result);
 }
+
+// TEST(CudaGaussian, Visual_Test){
+//     std::string image_path = std::string(PROJECT_DIR) + "/tests/test.jpg";
+//     Mat img = cv::imread(image_path, IMREAD_GRAYSCALE);
+//     short int* result;
+//     unsigned char* data = img.data;
+//     float sigma = 0.5;
+//     int rows = 256;
+//     int columns = 256;
+  
+//     int sum = 0;
+  
+//     gaussian(data,sigma,rows,columns,result);
+
+//     Mat resultMat(256,256, CV_16S, result);
+//     Mat result_display;
+
+//     normalize(resultMat, result_display, 0, 255, NORM_MINMAX);
+//     result_display.convertTo(result_display, CV_8U);
+
+//     imshow("CudaGaussian Visual Test", result_display);
+//     waitKey(0);
+// }
